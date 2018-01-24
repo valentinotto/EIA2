00acc4d4484c5fb087cfd2f7e8dea09da5efd7f4
valentinotto <32565438+valentinotto@users.noreply.github.com>
2018-01-24 12:14:26 +0100

    .

---
+++ 11_Node/DatabaseClient.js
@@ -0,0 +1,47 @@
+var DatabaseClient;
+(function (DatabaseClient) {
+    window.addEventListener("load", init);
+    function init(_event) {
+        console.log("Init");
+        let insertButton = document.getElementById("insert");
+        let refreshButton = document.getElementById("refresh");
+        insertButton.addEventListener("click", insert);
+        refreshButton.addEventListener("click", refresh);
+    }
+    function insert(_event) {
+        let inputs = document.getElementsByTagName("input");
+        let query = "command=insert";
+        query += "&name=" + inputs[0].value;
+        query += "&firstname=" + inputs[1].value;
+        query += "&matrikel=" + inputs[2].value;
+        console.log(query);
+        sendRequest(query, handleInsertResponse);
+    }
+    function refresh(_event) {
+        let query = "command=find";
+        sendRequest(query, handleFindResponse);
+    }
+    function sendRequest(_query, _callback) {
+        let xhr = new XMLHttpRequest();
+        //xhr.open("GET", "http://localhost:8100?" + _query, true);
+        xhr.open("GET", "https://eia2-w17-databasetest.herokuapp.com/?" + _query, true);
+        xhr.addEventListener("readystatechange", _callback);
+        xhr.send();
+    }
+    function handleInsertResponse(_event) {
+        let xhr = _event.target;
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            alert(xhr.response);
+        }
+    }
+    function handleFindResponse(_event) {
+        let xhr = _event.target;
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            let output = document.getElementsByTagName("textarea")[0];
+            output.value = xhr.response;
+            let responseAsJson = JSON.parse(xhr.response);
+            console.log(responseAsJson);
+        }
+    }
+})(DatabaseClient || (DatabaseClient = {}));
+//# sourceMappingURL=DatabaseClient.js.map

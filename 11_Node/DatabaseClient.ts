61f2c3de8f19d18b8a802be20651bf9f02ddf6b4
valentinotto <32565438+valentinotto@users.noreply.github.com>
2018-01-24 12:20:31 +0100

    .

--- 11_Node/DatabaseClient.ts
+++ 11_Node/DatabaseClient.ts
@@ -27,7 +27,7 @@ namespace DatabaseClient {
     function sendRequest(_query: string, _callback: EventListener): void {
         let xhr: XMLHttpRequest = new XMLHttpRequest();
         //xhr.open("GET", "http://localhost:8100?" + _query, true);
-        xhr.open("GET", "https://eia2-w17-databasetest.herokuapp.com/?" + _query, true);
+        xhr.open("GET", "https://eia2-db.herokuapp.com/?" + _query, true);
         xhr.addEventListener("readystatechange", _callback);
         xhr.send();
     }

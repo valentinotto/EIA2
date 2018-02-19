266524a1d33a4a20eda133c831b3bd8da51c865a
valentinotto <32565438+valentinotto@users.noreply.github.com>
2018-02-19 16:21:36 +0100

    .

--- 14/main.js
+++ 14/main.js
@@ -1,11 +1,21 @@
 var Aufgabe14;
 (function (Aufgabe14) {
-    window.addEventListener("load", draw);
-    function draw() {
+    /*
+    Aufgabe: 14
+    Name: Valentin Otto
+    Matrikel: 256320
+    Datum: 19.01.18
+  
+    Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+    */
+    window.addEventListener("load", screen1);
+    function screen1() {
         var canvas = document.getElementsByTagName("canvas")[0];
         var crc2 = canvas.getContext("2d");
-        // Himmel im Hintergrund
-        crc2.fillStyle = "black";
+        var grd = crc2.createLinearGradient(0, 800, 0, 0);
+        grd.addColorStop(0, "#999");
+        grd.addColorStop(1, "white");
+        crc2.fillStyle = grd;
         crc2.fillRect(0, 0, 400, 800);
     }
 })(Aufgabe14 || (Aufgabe14 = {}));
--- 14/main.ts
+++ 14/main.ts
@@ -1,13 +1,24 @@
 namespace Aufgabe14 {
-window.addEventListener("load", draw);
+  /*
+  Aufgabe: 14
+  Name: Valentin Otto
+  Matrikel: 256320
+  Datum: 19.01.18
 
-function draw() : void {
+  Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+  */
+
+window.addEventListener("load", screen1);
+
+function screen1() : void {
   let canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
 
   let crc2: CanvasRenderingContext2D = canvas.getContext("2d");
 
-// Himmel im Hintergrund
-  crc2.fillStyle = "black";
+  var grd = crc2.createLinearGradient(0,800,0,0);
+  grd.addColorStop(0,"#999");
+  grd.addColorStop(1,"white");
+  crc2.fillStyle = grd;
   crc2.fillRect(0, 0, 400, 800);
 }
 }
